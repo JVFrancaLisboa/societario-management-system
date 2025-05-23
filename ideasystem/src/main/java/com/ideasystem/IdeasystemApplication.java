9cd37d5baed72f0dc50245755037ae03c7f208fb
JVFrancaLisboa <logiccoder88@gmail.com>
2025-05-23 19:47:39 -0300

    Initial project

---
+++ ideasystem/src/main/java/com/ideasystem/IdeasystemApplication.java
@@ -0,0 +1,13 @@
+package com.ideasystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeasystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeasystemApplication.class, args);
+	}
+
+}
