72138fe5d9b96e60cfe57ec1486fee564e8dbc3d
JVFrancaLisboa <logiccoder88@gmail.com>
2025-05-29 12:59:17 -0300

    information required for clients registration

--- ideasystem/src/main/java/com/ideasystem/controllers/ClientController.java
+++ ideasystem/src/main/java/com/ideasystem/controllers/ClientController.java
@@ -1,9 +1,27 @@
 package com.ideasystem.controllers;
 
+import com.ideasystem.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/client")
+@RequestMapping("/clients")
 public class ClientController {
+
+    @Autowired
+    ClientService clientService;
+
+    @GetMapping
+    public String allClients(Model model){
+        model.addAttribute("clients", clientService.allClients());
+        return "screens/clients/all";
+    }
+
+    @GetMapping("/form")
+    public String clientForm(){
+        return "screens/clients/form";
+    }
 }
