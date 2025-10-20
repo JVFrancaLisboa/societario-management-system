a16b9e1b5a846b04f3e4b3e09e8b518a82e202e0
JVFrancaLisboa <logiccoder88@gmail.com>
2025-10-20 14:26:31 -0300

    feat: certificate class created an modeled

---
+++ ideasystem/src/main/java/com/ideasystem/entities/Certificate.java
@@ -0,0 +1,30 @@
+package com.ideasystem.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Table(name = "certificates")
+public class Certificate {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String razaoSocial;
+
+    @Column
+    private LocalDate dataValidade;
+
+    @Column(length = 20)
+    private String contato;
+
+    @Column(length = 20)
+    private String whatsapp;
+
+    @Column(length = 200)
+    private String email;
+}
